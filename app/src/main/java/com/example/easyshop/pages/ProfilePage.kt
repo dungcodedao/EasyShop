2298e912a0ced193268b1b325469d7359026364c
Ngô Văn Dũng <ngovandung1552003@gmail.com>
2025-11-20 22:26:34 +0700

    update: add file

--- app/src/main/java/com/example/easyshop/pages/ProfilePage.kt
+++ app/src/main/java/com/example/easyshop/pages/ProfilePage.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
