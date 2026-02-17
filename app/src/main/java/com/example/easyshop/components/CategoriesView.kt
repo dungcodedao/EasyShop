3edbdd74a2654d423064672382320f6994564a19
DevDus <ngovandung1552003@gmail.com>
2026-02-17 21:44:13 +0700

    sua icon danh muc san pham

--- app/src/main/java/com/example/easyshop/components/CategoriesView.kt
+++ app/src/main/java/com/example/easyshop/components/CategoriesView.kt
@@ -1,11 +1,7 @@
 package com.example.easyshop.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,7 +9,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
